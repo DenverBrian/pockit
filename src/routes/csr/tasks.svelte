79911e05074bbe25c0f900aa03d4c232dbf35a16
Artifex <markasena@gmail.com>
2023-10-08 18:50:36 +0800

    wip

--- src/routes/csr/tasks.svelte
+++ src/routes/csr/tasks.svelte
@@ -3,12 +3,35 @@
 	import { db } from '$lib/db';
 	import type { Task } from '$lib/types';
 	import Stats from './stats.svelte';
+	import { get } from 'svelte/store';
 	import Taskstatus from './taskstatus.svelte';
 	const getUserTasks = async () => {
 		const taskList = (await db
 			.collection('tasks')
 			.getFullList({ sort: '-created' })) as Task[];
 		tasksStore.update(() => taskList);
+	};
+
+	const handleTaskClick = async (id: string) => {
+		const task = get(tasksStore).find((task) => task.id === id);
+		if (task) {
+			if (task.status === 'done') {
+				task.status = 'todo';
+			} else if (task.status === 'todo') {
+				task.status = 'inProgress';
+			} else {
+				task.status = 'done';
+			}
+			tasksStore.update((tasks) => {
+				tasks.find((t) => {
+					if (t.id === id) {
+						t.status = task.status;
+					}
+				});
+				return tasks;
+			});
+			await db.collection('tasks').update(id, task);
+		}
 	};
 </script>
 
@@ -20,10 +43,14 @@
 		{:then}
 			{#each $tasksStore as task}
 				<p>
-					{task.description} - <Taskstatus status={task.status} />
+					{task.description} -
+					<button
+						on:click={() => handleTaskClick(task.id)}
+						style="background:transparent ; border-color: transparent;"
+						><Taskstatus status={task.status} /></button
+					>
 				</p>
 			{/each}
 		{/await}
 	{/if}
-	<!-- <Stats /> -->
 </div>
